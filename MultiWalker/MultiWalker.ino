c026086afb06394df46bdf2bc34f6b8357ee5668
bgklug <bgklug@alaska.edu>
2015-12-09 09:29:25 -0900

    Working Base code

--- MultiWalker/MultiWalker.ino
+++ MultiWalker/MultiWalker.ino
@@ -4,7 +4,9 @@ class Leg
 {
 public:
   // ctor
-  Leg(int hipPin, int thighPin, int footPin){
+  Leg(){}
+
+  void activate(int hipPin, int thighPin, int footPin){
     hip.attach(hipPin);
     thigh.attach(thighPin);
     foot.attach(footPin);
@@ -15,26 +17,25 @@ public:
     foot.write(90);
     delay(200);
   }
-
   // Member Functions
   void wave(){
     for(int i = 0; i < 180; ++i)
     {
       thigh.write(i);
       foot.write(i);
-      delay(100);
+      delay(20);
     }
     for(int i = 0; i < 180; ++i)
     {
       thigh.write(180 - i);
       foot.write(180 - i);
-      delay(100);
+      delay(20);
     }
-    for(int i = 0; i < 30; ++i)
+    for(int i = 0; i < 70; ++i)
     {
       thigh.write(i);
       foot.write(i);
-      delay(100);
+      delay(20);
     }
   }
   
@@ -46,33 +47,75 @@ public:
       for(int i = hipStart; i < 90; ++i)
       {
         hip.write(i);
-        delay(500);
+        delay(20);
       }
     }else{
       for(int i = hipStart; i > 90; --i)
       {
         hip.write(i);
-        delay(500);
+        delay(20);
       }
     }
     if(footStart < 30){
-      for(int i = footStart; i < 30; ++i)
+      for(int i = footStart; i < 70; ++i)
       {
         foot.write(i);
-        delay(500);
+        delay(20);
       }
     }else{
-      for(int i = footStart; i > 30; --i)
+      for(int i = footStart; i > 70; --i)
       {
         foot.write(i);
-        delay(500);
+        delay(20);
       }
     }
     for(int i = thighStart; i > 0; --i)
     {
       thigh.write(i);
-      delay(500);
+      delay(20);
     }
+  }
+
+  // Sends legs to low crouch position
+  // hip = 90
+  // thigh = 
+  void crouch(){
+    
+    char hipStart = hip.read();
+    char footStart = foot.read();
+    char thighStart = thigh.read();
+    if(hipStart < 90){
+      for(int i = hipStart; i < 90; ++i)
+      {
+        hip.write(i);
+        delay(20);
+      }
+    }else{
+      for(int i = hipStart; i > 90; --i)
+      {
+        hip.write(i);
+        delay(20);
+      }
+    }
+    if(footStart < 30){
+      for(int i = footStart; i < 70; ++i)
+      {
+        foot.write(i);
+        delay(20);
+      }
+    }else{
+      for(int i = footStart; i > 70; --i)
+      {
+        foot.write(i);
+        delay(20);
+      }
+    }
+    for(int i = thighStart; i > 0; --i)
+    {
+      thigh.write(i);
+      delay(20);
+    }
+  }
   }
 
   // Private Data Members
@@ -92,36 +135,39 @@ private:
   Servo foot;
 };
 
-Leg legOne(13,10,9);
-/*Leg legTwo(8,7,6);
-Leg legThree(5,4,3);
-Leg legFour(2,1,0);
-Leg legFive(15,16,17);
-Leg legSix(18,19,20);*/
+Leg legOne;
+Leg legTwo;
+Leg legThree;
+Leg legFour;
+Leg legFive;
+Leg legSix;
 
 void setup() {
-  legOne.stand();
-  /*legTwo.stand();
-  delay(2000);
-  legThree.stand();
-  legFour.stand();
-  delay(2000);
-  legFive.stand();
-  legSix.stand();
-  delay(2000);*/
+  Serial.begin(9600);
+  Serial.println("Starting Set up");
+  legOne.activate(13,10,9);
+  legTwo.activate(8,7,6);
+  legThree.activate(5,4,3);
+  legFour.activate(26,28,30);
+  legFive.activate(15,16,17);
+  legSix.activate(18,19,20);
+  Serial.println("Setup Complete");
 }
 
 void loop() {
- /* legOne.stand();
+  Serial.println("Leg one and two standing");
+  legOne.stand();
   legTwo.stand();
   delay(2000);
+  Serial.println("Leg three and four standing");
   legThree.stand();
   legFour.stand();
   delay(2000);
+  Serial.println("Leg five and six standing");
   legFive.stand();
   legSix.stand();
-  delay(2000);*/
-
+  
+  Serial.println("Leg one Waves");
   legOne.wave();
   delay(1000);
 }
